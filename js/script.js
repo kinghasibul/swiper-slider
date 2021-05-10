96a0495b294a6bdb510434730aad7155d8ba254b
King Hasibul <83134617+kinghasibul@users.noreply.github.com>
2021-05-10 20:20:43 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    
+   const swiper = new Swiper('.swiper-container', { 
+        effect: 'cube',
+        grabCursor: true,
+        cubeEffect: {
+            shadow: true,
+            slideShadows: true,
+            shadowOffset: 20,
+            shadowScale: 0.94,
+        },
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        speed:1000,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'fraction',
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        scrollbar: {
+           el: '.swiper-scrollbar',
+            hide: 'true',
+        },
+       
+       
+   });
+    
+});
